db209ef1e6b08b6ac33b82fd9570cd313f924097
Federico Gelabert <gelabert.fede@gmail.com>
2023-11-01 23:18:54 -0300

    Fix: BannerTour spacing.

--- src/DigitalOcean/components/BannerTour.jsx
+++ src/DigitalOcean/components/BannerTour.jsx
@@ -4,7 +4,7 @@ const BannerTour = () => {
         <div className="container mx-auto flex justify-center mt-48 mb-32 xl:px-28">
             <div className="flex text-white relative lg:h-52 h-120 w-10/12 rounded-3xl"
              style={{ background: "rgb(20, 51, 214)", backgroundImage: "url('https://www.digitalocean.com/_next/static/media/banner-background.f302a2e4.svg')", backgroundRepeat: "no-repeat", backgroundPosition: "right bottom" }}>
-                <div className="flex flex-col lg:justify-between py-8 lg:pl-16 pl-12">
+                <div className="flex flex-col lg:justify-between py-8 lg:pl-16 mob:pl-12 pl-8">
                     <h4 className="text-3xl font-bold font-epilogue lg:pb-0 pb-5 w-5/6">Quick product tours</h4>
                     <p className="max-w-xl lg:pb-0 pb-5 w-11/12">Check out our brief product tours to see how simple, easy, and pleasant it is to use DigitalOcean's cloud hosting products.</p>
                     <a href="#" className="font-medium flex items-center font-jetbrain">Take a tour<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 ml-2">
